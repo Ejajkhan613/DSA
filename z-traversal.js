0648badbe235d5938503fad310df83981a8424f3
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2025-05-15 21:57:13 +0530

    z traversal

---
+++ z-traversal.js
@@ -0,0 +1,41 @@
+let N = 4;
+
+let matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+
+
+function zTraversal(N, matrix) {
+    let str = "";
+
+    let i = 0;
+    let j = 0;
+
+    while(j < N){
+        str += matrix[i][j] + " ";
+        j++;
+    }
+
+    i = 1;
+    j = N-2;
+    while(i < N){
+        str += matrix[i][j] + " ";
+        i++;
+        j--;
+    }
+
+    i = N-1;
+    j = 1;
+    while(j < N){
+        str += matrix[i][j] + " ";
+        j++;
+    }
+
+    console.log(str);
+}
+
+zTraversal(N, matrix);
